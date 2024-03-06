5927a484619efb1eb3a83f5fc8bd03a07b6dedd1
ShwetaBanerjee23 <shwetabanerjee23@icloud.com>
2024-03-06 07:22:18 +0000

    Starting point.

--- image_classification.py
+++ image_classification.py
@@ -87,36 +87,14 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # Train the model.
-for epoch in range(2):  # Loop over the dataset multiple times.
-    running_loss = 0.0
-    
-    for i, data in enumerate(train_loader, 0):
-        # Get the inputs; data is a list of [inputs, labels].
-        inputs, labels = data
-
-        # Zero the parameter gradients.
-        optimizer.zero_grad()
-
-        # Forward + backward + optimize.
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # Print statistics.
-        running_loss += loss.item()
-        if i % 2000 == 1999:  # Print every 2000 mini-batches.
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, 
-                                            i + 1, 
-                                            running_loss / 2000))
-            running_loss = 0.0
+#### TODO ####
 
 print('Finished Training')
 
 # Evaluate the model.
 correct = 0
 total = 0
-
+``
 with torch.no_grad():
     for data in test_loader:
         images, labels = data
